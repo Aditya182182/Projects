560cdf2380e8bfb8459e9c4b7b7e55e58c4e25ce
Aditya <adinayak182@gmail.com>
2024-05-03 14:28:36 +0530

    commit1

--- Marksheet.py
+++ Marksheet.py
@@ -1,33 +1,33 @@
 name = input("Enter student name: ")
 add = input("Enter student address: ")
 
-subjects = []
+subjects = {}
+all_marks = []
 for i in range(5):
     subject_name = input("Enter subject {} name: ".format(i + 1))
-    marks = float(input("Enter marks obtained in {}: ".format(subject_name)))
-    subjects.append((subject_name, marks))
+    marks = int(input("Enter marks obtained in {}: ".format(subject_name)))
+    print(type(marks))
+    subjects[subject_name] = marks
+    all_marks.append(marks)
 
-total_marks = 500
-obtained_marks = 0
-for subject, marks in subjects:
-    total_marks += 100  
-    obtained_marks += marks
-# total_marks=500   
-# obt_marks=subject_name
-# percentage=obt_marks/total_marks*100
 
-# percentage = (obt_marks / total_marks) * 100
+
+total_marks=500   
+obt_marks=sum(all_marks)
+percentage=obt_marks/total_marks*100
+percentage = (obt_marks / total_marks) * 100
+
+
+
 print("                                                Your name :", name)
 print("                                                Address: ",add)
 print("============================================================================================================================")
 print("Subject                    |Total Marks           |Obtain Marks                           |Percentage")
 print("============================================================================================================================")
-print("Maths                      |100                   |",subject,"                                 |",marks)
-print("Physics                    |100                   |",subject,"                                 |",marks)
-print("Chemistry                  |100                   |",subject,"                                 |",marks)
-print("Biology                    |100                   |",subject,"                                 |",marks)
-print("English                    |100                   |",subject,"                                 |",marks)
+for subject_name, marks in subjects.items():
+    print(f"{subject_name}               |100                   |{marks}                                  |{marks/100*100}")
+
 print("============================================================================================================================")
-print("Total                      |500                   |",obtained_marks ,"                                |",percentage)
+print("Total                      |500                   |",obt_marks ,"                                |",percentage)
 
 
